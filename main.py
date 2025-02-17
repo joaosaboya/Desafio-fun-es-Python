66255e9fea8b55fcfd3a31e9420203d11e9115c9
Joao_Saboya <joaosaboya123@gmail.com>
2025-02-17 15:27:06 -0300

    Second commit

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ else:
 ## Missão 3: Recuperando o Sistema de Notas
 
 notas = int(input('Digite sua nota: '))   
-if notas >= 90 and notas <= 100:
+if notas <= 100 and notas >= 90:
     print('Parabéns, você tirou A!')
-elif notas >= 89 and notas <= 80:
+elif notas <= 89 and notas >= 80:
     print('Muito bem, você tirou B!')    
-elif notas >= 79 and notas <= 70:
+elif notas <= 79 and notas >= 70:
     print('Bom trabalho, você tirou C!')    
-elif notas >= 69 and notas <= 60:
+elif notas <= 69 and notas >= 60:
     print('Fique atento, você tirou D!')    
 else:
     print('Estude um pouco mais, você tirou F!')
@@ -48,7 +48,7 @@ print(resultado)
 login = input('Digite seu login: ')
 senha = input('Digite sua senha: ')
 
-if login () and senha == 'Python123':
+if senha == 'Python123':
     print('Senha correta!')
 else:
     print('Senha incorreta!')    
@@ -84,7 +84,11 @@ print(alunos[4])
 def dobro (numero):
     return (numero * 2)
 
-numero = (input('Digite um número: '))
-resultado(numero * 2)
-print(resultado)
+numero = int(input('Digite um número: '))
 
+print(numero * 2)
+
+## Missão 10: Contando Letras
+
+contar_letras = input('Digite seu nome: ')
+print(len(contar_letras))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,11 @@
-contar_letras = input('Digite seu nome: ')
-print(len(contar_letras))
+notas = int(input('Digite sua nota: '))   
+if notas <= 100 and notas >= 90:
+    print('Parabéns, você tirou A!')
+elif notas <= 89 and notas >= 80:
+    print('Muito bem, você tirou B!')    
+elif notas <= 79 and notas >= 70:
+    print('Bom trabalho, você tirou C!')    
+elif notas <= 69 and notas >= 60:
+    print('Fique atento, você tirou D!')    
+else:
+    print('Estude um pouco mais, você tirou F!')
